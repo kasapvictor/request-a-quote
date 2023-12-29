fa150962cf70010b1ac8ca8d08e1ac8413d816e2
kasapvictor <kasap.victor@gmail.com>
2023-12-29 07:26:50 -0800

    feat: Added prev-next

--- src/widgets/request-quote-form/index.ts
+++ src/widgets/request-quote-form/index.ts
@@ -1,7 +1,9 @@
 import { initInputZipFrom } from '@features/input-zip-from';
 import { initInputZipTo } from '@features/input-zip-to';
+import { initPrevNext } from '@features/prev-next';
 
 export const initRequestQuoteForm = () => {
+  initPrevNext();
   initInputZipFrom();
   initInputZipTo();
 };
